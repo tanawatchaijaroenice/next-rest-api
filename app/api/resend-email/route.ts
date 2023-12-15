335741dff0fa5605859b77aa8491c6926b3ddbca
ICE\tanawat <tanawat@smartfinder.asia>
2023-12-15 10:55:17 +0700

    .env next public

--- app/api/resend-email/route.ts
+++ app/api/resend-email/route.ts
@@ -1,7 +1,7 @@
 // import { EmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(request: Request) {
     const res = await request.json()
