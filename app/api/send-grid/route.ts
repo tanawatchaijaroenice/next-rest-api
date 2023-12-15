2330e82154ab950d33fcca23d17504542662deee
ICE\tanawat <tanawat@smartfinder.asia>
2023-12-15 16:43:56 +0700

    send grid old

--- app/api/send-grid/route.ts
+++ app/api/send-grid/route.ts
@@ -1,28 +1,45 @@
 import { NextResponse } from "next/server";
 
-// import sendgrid from "@sendgrid/mail";
+import sendgrid from "@sendgrid/mail";
 
-// sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASS,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+});
 
-// export async function POST(request: Request) {
-//     try {
-//         const res = await request.json()
-//         const { name, email } = res;
+export async function POST(request: Request) {
+    try {
+        const res = await request.json()
+        const { name, email } = res;
 
-//         await sendgrid.send({
-//             from: 'tanawat.chaijaroenice@gmail.com', // your website email address here
-//             to: email, // Your email where you'll receive emails
-//             subject: `${name}`,
-//             html: `<div>You've got a mail</div>`,
-//         });
+        connection.query(
+            'SELECT Idmail, Usermail, Tokenmail, Servicemail, Hostmail, Securemail, Apikey FROM mail_config WHERE Idmail = 2',
+            async (error: any, result: any) => {
+                if (error) {
+                    console.log("Select error", error);
+                    return NextResponse.json({ message: 'ERROR', error }, { status: 500 });
+                }
+                console.log("result", result);
+                sendgrid.setApiKey(result[0].Apikey);
 
-//     } catch (error) {
-//         // console.log(error);
-//         return NextResponse.json({ error }, { status: 500 });
-//     }
+                await sendgrid.send({
+                    from: 'tanawat.chaijaroenice@gmail.com', // your website email address here
+                    to: email, // Your email where you'll receive emails
+                    subject: `${name}`,
+                    html: `<div>You've got a mail</div>`,
+                });
+            })
+        return NextResponse.json({ message: 'sent mail success!!' });
+    } catch (error) {
+        // console.log(error);
+        return NextResponse.json({ error }, { status: 500 });
+    }
 
-//     return NextResponse.json({ message: 'success' });
-// }
+}
 
 
 
@@ -42,52 +59,51 @@ import { NextResponse } from "next/server";
 // }
 
 
-import sgMail from "@sendgrid/mail";
-// require("dotenv").config();
+// import sgMail from "@sendgrid/mail";
 
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASS,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-});
+// const mysql = require('mysql2');
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     password: process.env.DB_PASS,
+//     user: process.env.DB_USER,
+//     database: process.env.DB_DATABASE,
+// });
 
-export async function POST(request: Request) {
-    const res = await request.json()
-    const { name, email } = res;
+// export async function POST(request: Request) {
+//     const res = await request.json()
+//     const { name, email } = res;
 
-    try {
-        connection.query(
-            'SELECT Idmail, Usermail, Tokenmail, Servicemail, Hostmail, Securemail, Apikey FROM mail_config WHERE Idmail = 2',
-            async (error: any, result: any) => {
-                if (error) {
-                    console.log("Select error", error);
-                    return NextResponse.json({ message: 'ERROR', error }, { status: 500 });
-                }
-                console.log("result", result);
-                const msg = {
-                    from: 'tanawat.chaijaroenice@gmail.com',
-                    to: email, // Replace with your own email address
-                    subject: "New message from your website",
-                    text: 'Hello World' + name,
-                };
+//     try {
+//         connection.query(
+//             'SELECT Idmail, Usermail, Tokenmail, Servicemail, Hostmail, Securemail, Apikey FROM mail_config WHERE Idmail = 2',
+//             async (error: any, result: any) => {
+//                 if (error) {
+//                     console.log("Select error", error);
+//                     return NextResponse.json({ message: 'ERROR', error }, { status: 500 });
+//                 }
+//                 console.log("result", result);
+//                 const msg = {
+//                     from: 'tanawat.chaijaroenice@gmail.com',
+//                     to: email, // Replace with your own email address
+//                     subject: "New message from your website",
+//                     text: 'Hello World' + name,
+//                 };
 
-                var SEND_GRID_API_KEY = result[0].Apikey
+//                 var SEND_GRID_API_KEY = result[0].Apikey
 
-                sgMail.setApiKey(SEND_GRID_API_KEY);
+//                 sgMail.setApiKey(SEND_GRID_API_KEY);
 
-                try {
-                    await sgMail.send(msg);
-                } catch (error) {
-                    console.error(error);
-                    return NextResponse.json({ message: "Something went wrong." });
-                }
-            })
-            return NextResponse.json({ message: "Email sent successfully!" });
-    } catch (error) {
-        return NextResponse.json({ error });
-    }
+//                 try {
+//                     await sgMail.send(msg);
+//                 } catch (error) {
+//                     console.error(error);
+//                     return NextResponse.json({ message: "Something went wrong." });
+//                 }
+//             })
+//             return NextResponse.json({ message: "Email sent successfully!" });
+//     } catch (error) {
+//         return NextResponse.json({ error });
+//     }
 
-}
+// }
